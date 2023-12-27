1a45cd6208669aa049e9e922828f3e8c232e8700
fvrrrn <boris.chernystrand@gmail.com>
2023-12-27 16:56:28 +0300

    lang(js): add 1980 find unique binary string

---
+++ problems/1980/1980.js
@@ -0,0 +1,14 @@
+/**
+ *
+ * @param {string[]} nums
+ * @returns {string}
+ */
+function findDifferentBinaryString(nums) {
+  let result = "";
+
+  for (let i = 0; i < nums.length; ++i) {
+    result += nums[i][i] === "0" ? "1" : "0";
+  }
+
+  return result;
+}
